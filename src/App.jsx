59c008a0e3c342596eb15c811d0c6ae90f8595c2
Cristian Hernandez <losreyes2504@gmail.com>
2025-02-22 23:40:28 -0600

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { TokenProvider } from './context/TokenContext'; // Importamos el TokenProvider
 import NavbarComponent from './components/Navbar';
-import { Login } from './pages/Login';
-import { Home } from './pages/Home';
+import { Login } from './pages/Login/index';
+import { Home } from './pages/Home/index';
 import { ThemeProvider } from '@mui/material';
 import theme from './theme/theme'; // Importa el tema desde la carpeta theme
 
@@ -12,11 +12,10 @@ function App() {
     <TokenProvider>
       <ThemeProvider theme={theme}> {/* Aplica el tema aquí */}
       <Router>
-  
         <NavbarComponent />
         <Routes>
-          <Route path="/login" element={<Login />} />
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </Router>
       </ThemeProvider>
